adf248bd96d20a270751de32326a24ac66dbe375
HenrikJava <henriklarssonmail@gmail.com>
2020-04-27 16:23:34 +0200

    Created the nodes in network class

--- src/routeCity/Network.java
+++ src/routeCity/Network.java
@@ -1,12 +1,13 @@
 package routeCity;
 
-public class Network implements Dijkstras{
+public class Network implements Dijkstras {
     /* static variables & methods */
-    private static Network instance = new Network();
-
-    public static final String[] BUS_STATIONS = new String[10];
-
-    public static Network getInstance(){return instance;}
+    public static String[] BUS_STATIONS = {"Kungsgatan", "Centralstationen", "Drottningtorget", "Hamngatan",
+            "Vasaplatsen", "Sportarenan", "Flygplatsen", "Södra vägen", "Musikvägen", "Kulturvägen"};
+    private static Network instance = new Network(2,3,Network.BUS_STATIONS);
+    public static Network getInstance() {
+        return instance;
+    }
 
     /*  >Member variables<  */
 
@@ -14,13 +15,39 @@ public class Network implements Dijkstras{
 
     /*  >>Constructors<<  */
 
-    private Network(){}
+    private Network(int min,int max,String[] nodesName) {
+        remake(min,max,nodesName);
+    }
 
     /*  >>>Member methods<<<  */
-    private void setRandomRelations(int min,int max){}
-    private int getRandomWeight(int min,int max){return -1;}
-    private boolean isClosed(){return false;}
-    public String getShortestPath(Node start,Node end){return null;}
-    public void printNetwork(){}
-    public void remake(){}
+    private void setRandomRelations(int min, int max) {
+    }
+
+    private int getRandomWeight(int min, int max) {
+        return -1;
+    }
+
+    private boolean isClosed() {
+        return false;
+    }
+
+    public String getShortestPath(Node start, Node end) {
+        return null;
+    }
+
+    public void printNetwork() {
+    }
+
+    public void remake(int min,int max,String[] nodesName) {
+        nodes = new Node[nodesName.length];
+        for (int i = 0; i <nodes.length; i++) {
+            nodes[i] = new Node(getNodeSymbol(i), BUS_STATIONS[i]);
+        }
+        setRandomRelations(min,max);
+    }
+
+    public String getNodeSymbol(int index) {
+        char letter = (char) (index % 26+65);
+        return letter + ""+(index / 26 + 1);
+    }
 }
